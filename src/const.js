f76e08c5f30fc4d8ae0b9642ed91509da0fc7678
Maria <itimasha@gmail.com>
2020-01-27 22:09:41 +0300

    done

--- src/const.js
+++ src/const.js
@@ -1,8 +1,22 @@
-export const COLORS = [`black`, `yellow`, `blue`, `green`, `pink`];
+export const COLOR = {
+  BLACK: `black`,
+  YELLOW: `yellow`,
+  BLUE: `blue`,
+  GREEN: `green`,
+  PINK: `pink`,
+};
+
+export const COLORS = [
+  COLOR.BLACK,
+  COLOR.YELLOW,
+  COLOR.BLUE,
+  COLOR.GREEN,
+  COLOR.PINK
+];
 
 export const DAYS = [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`];
 
-export const MONTHS = [
+export const MONTH_NAMES = [
   `January`,
   `February`,
   `March`,
@@ -16,3 +30,13 @@ export const MONTHS = [
   `November`,
   `December`,
 ];
+
+export const FilterType = {
+  ALL: `all`,
+  ARCHIVE: `archive`,
+  FAVORITES: `favorites`,
+  OVERDUE: `overdue`,
+  REPEATING: `repeating`,
+  TAGS: `tags`,
+  TODAY: `today`,
+};
